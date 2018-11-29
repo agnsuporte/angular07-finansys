d28727fab2d116b1b6c6e074b958c2647e7e69e7
agnsuporte <agnsuporte@gmail.com>
2018-11-29 12:46:25 -0300

    correção de bugs

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -3,10 +3,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Observable, throwError} from 'rxjs';
-// import { map, catchError, flatMap } from 'rxjs/operators';
-import { map, catchError } from 'rxjs/operators';
+import { map, catchError, flatMap } from 'rxjs/operators';
 
 import { Entry } from './entry.module';
+import { CategoryService } from '../../categories/shared/category.service';
 
 @Injectable({
   providedIn: 'root'
@@ -15,7 +15,7 @@ export class EntryService {
 
   private apiPath = 'api/entries';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private categoryService: CategoryService) { }
 
   getAll(): Observable<Entry[]> {
     return this.http.get(this.apiPath).pipe(
@@ -33,17 +33,50 @@ export class EntryService {
   }
 
   create(entry: Entry): Observable<Entry> {
-    return this.http.post(this.apiPath, entry).pipe(
-      catchError(this.handleError),
-      map(this.jsonDataToEntry)
+
+    // return this.http.post(this.apiPath, entry).pipe(
+    //   catchError(this.handleError),
+    //   map(this.jsonDataToEntry)
+    // );
+
+    /**
+     * Isto para suprir a necessidade do in-memory-database que
+     * nao recebe a categoria do objeto recem criado.
+     */
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap( category => {
+        entry.category = category;
+
+        return this.http.post(this.apiPath, entry).pipe(
+          catchError(this.handleError),
+          map(this.jsonDataToEntry)
+        );
+      })
     );
+
   }
 
   update(entry: Entry): Observable<Entry> {
     const url = `${this.apiPath}/${entry.id}`;
-    return this.http.put(url, entry).pipe(
-      catchError(this.handleError),
-      map(() => entry)
+
+    // return this.http.put(url, entry).pipe(
+    //   catchError(this.handleError),
+    //   map(() => entry)
+    // );
+
+    /**
+     * Isto para suprir a necessidade do in-memory-database que
+     * nao recebe a categoria do objeto recem criado.
+    */
+    return this.categoryService.getById(entry.categoryId).pipe(
+      flatMap( category => {
+        entry.category = category;
+
+        return this.http.put(url, entry).pipe(
+          catchError(this.handleError),
+          map(() => entry)
+        );
+      })
     );
   }
 
